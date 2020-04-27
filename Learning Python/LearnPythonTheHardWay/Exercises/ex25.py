20edeced75ab55c9e36bbcd7b6123b9c9f4278ad
kevwill79 <dmx36502@yahoo.com>
2020-04-26 17:11:34 -0700

    added exercises 21, 24-27, 29-30

---
+++ Learning Python/LearnPythonTheHardWay/Exercises/ex25.py
@@ -0,0 +1,55 @@
+def break_words(stuff):
+    """"This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+#Used str.lower so all words would be lowercase before sorting
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words, key=str.lower)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print(word)
+
+def print_last_word(words):
+    """"Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print(word)
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+    
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+sentence = "This is my test sentence!"
+
+#Function calls
+words = break_words(sentence)
+print(words)
+
+sorted_words = sort_words(words)
+print(sorted_words)
+
+print_first_word(words)
+print_last_word(words)
+
+sorted_sentence = sort_sentence(sentence)
+print(sorted_sentence)
+
+print_first_and_last(sentence)
+
+print_first_and_last_sorted(sentence)
---
+++ Learning Python/helloWorld.py
@@ -0,0 +1,6 @@
+#Let's get this party started with everyone's favorite Hello World!
+
+your_name_here = input(print("What's your name?"))
+
+print("%s tell everyone Hello, World!" % your_name_here)
+
